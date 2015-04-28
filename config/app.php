0966f458ef229f2f0d7cfc79056d063c9696f73d
tizz98 <dev.tizz98@gmail.com>
2015-04-28 22:22:03 +0000

    email registration

--- config/app.php
+++ config/app.php
@@ -136,6 +136,7 @@ return [
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Html\HtmlServiceProvider',
+		'Laracasts\Flash\FlashServiceProvider',
 
 		/*
 		 * Application Service Providers...
@@ -195,6 +196,7 @@ return [
 		'View'      => 'Illuminate\Support\Facades\View',
 		'Form'      => 'Illuminate\Html\FormFacade',
 		'HTML'      => 'Illuminate\Html\HtmlFacade',
+		'Flash'     => 'Laracasts\Flash\Flash',
 
 	],
 
