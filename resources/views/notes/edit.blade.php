98141c846ef7f8b8fa9ea8cb9e82a3dd33282fcc
tizz98 <dev.tizz98@gmail.com>
2015-05-06 02:48:37 +0000

    fixed route for editing note

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -10,7 +10,7 @@
 <h2>Edit {{ $note->title }}</h2><br>
 @include('flash::message')
 @include('layouts.partials._errors')
-{!! Form::open(['route' => 'notes.store']) !!}
+{!! Form::open(['route' => ['notes.update', $note->id], 'method' => 'PATCH']) !!}
 	@include('notes.partials._form', ['title_val' => $note->title, 'collection_val' => $note->collection_id, 'body_text_val' => $note->body_text, 'is_public_val' => $is_public])
 {!! Form::close() !!}
 @endsection
