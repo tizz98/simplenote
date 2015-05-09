e625aed5791f224053e11bbb7fb93e25cdae362b
tizz98 <dev.tizz98@gmail.com>
2015-05-09 17:24:55 +0000

    unused comments

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -9,19 +9,16 @@
 
 {!! Form::open(['route' => 'login_path']) !!}
 
-<!--- Username Field --->
 <div class="form-group">
     {!! Form::label('username', 'Username:') !!}
     {!! Form::text('username', $username, ['class' => 'form-control', 'required' => 'required']) !!}
 </div>
 
-<!--- Password Field --->
 <div class="form-group">
     {!! Form::label('password', 'Password:') !!}
     {!! Form::password('password', ['class' => 'form-control', 'required' => 'required']) !!}
 </div>
 
-<!--- Sign In Field --->
 <div class="form-group">
     {!! Form::submit('Sign In', ['class' => 'btn btn-primary']) !!}
     <a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
