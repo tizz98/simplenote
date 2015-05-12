538553e5016dc87e9ed485e23915c44258101fa6
tizz98 <dev.tizz98@gmail.com>
2015-05-12 04:08:22 +0000

    base ember js work

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,16 @@
 App = Ember.Application.create({
     LOG_TRANSITIONS: true
 });
+
+App.Router.map(function(){
+	this.resource('register');
+	this.resource('login');
+});
+
+Ember.LinkView.reopen({
+	attributeBindings: ['role']
+});
+
+Ember.Router.extend({
+  location: 'history'
+});
