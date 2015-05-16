9991ed3aa7ea155610b430324f3107bbf3837f00
tizz98 <dev.tizz98@gmail.com>
2015-05-16 05:08:47 +0000

    paginate notes in a collection

--- app/Http/Controllers/CollectionsController.php
+++ app/Http/Controllers/CollectionsController.php
@@ -93,23 +93,12 @@ class CollectionsController extends Controller {
 			}
 		}
 
-		$temp_notes = array();
-
-		for ($i=0; $i < count($collection->notes); $i++) { 
-			$note = $collection->notes[$i];
-
-			if ($note->is_public || ($note->user == Auth::User())) {
-				$note->shortText = substr($note->body_text, 0, 500);
-				array_push($temp_notes, $note);
-			}
-		}
-
 		$temp_str = 'collections/' . $id;
 		$active_triggers = [$temp_str];
 
-		$collection->notes = $temp_notes;
+		$notes = $collection->notes()->paginate(5);
 
-		return view('collections.show', compact('collection', 'active_triggers'));
+		return view('collections.show', compact('collection', 'notes', 'active_triggers'));
 	}
 
 	/**
